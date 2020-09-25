3b3c80d7e13f8b61fffa4aed9076c2161e1432fa
DashaDidyk <dashka.didyk@gmail.com>
2020-09-25 18:37:13 +0300

    first commit

---
+++ item.js
@@ -0,0 +1,30 @@
+class Item {
+  constructor(content) {
+    this.content = content;
+    this.state = false;
+    this._items = [];
+    this.el = document.createElement('div');
+    this.registerListeners();
+    this.setContent(content);
+  }
+
+  registerListeners() {
+    this.el.addEventListener('click', () => {
+      this.toggleState();
+    });
+  }
+
+  setContent(content) {
+    this.el.innerText = content;
+  }
+
+  addItem(item) {
+    this._items = [...this._items, item];
+    this.el.append(item.el);
+  }
+
+  toggleState() {
+    this.state = !this.state;
+    this.el.classList.toggle('crossed');
+  }
+}
